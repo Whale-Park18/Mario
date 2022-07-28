73f59b9189295a2039684483aef797d322374be0
Whale-Park18 <whale.park18@gmail.com>
2022-07-29 01:53:11 +0900

    [수정] Mario.Weapon 스크립트 패키지 * [추가] 미사일 스크립트

---
+++ Assets/Scripts/Monobehaviour/Weapon/Missile.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float _speed;
+    public float _maxSpeed;
+    Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(_rigidbody.velocity.magnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        if (_rigidbody.velocity.magnitude >= _maxSpeed * _maxSpeed)
+            return;
+
+        _rigidbody.AddForce(transform.forward * _speed, ForceMode.Impulse);
+    }
+}
